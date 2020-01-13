7ddd8b683a58b23e6c0e2023b85f80c1b55702ee
MAwais <mawais54013@outlook.com>
2020-01-13 15:26:34 -0800

    added image

--- index.vr.js
+++ index.vr.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react';
 
+import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
+
+class WeatherSimulator extends Component {
+  render() {
+    return (
+      <View>
+        <Pano>
+          
+        </Pano>
+      </View>
+    )
+  }
+}
